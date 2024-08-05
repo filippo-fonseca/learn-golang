0d06f846039259a0a7b447eaba3ed11f5109cd32
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-05 09:37:21 -0600

    fizzbuzz

--- basics/loops/loops.go
+++ basics/loops/loops.go
@@ -1,31 +1,33 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func bulkSend(numMessages int) float64 {
+// 	sum := 0.0
+// 	for i := 0; i < numMessages; i++ {
+// 		sum += 1.0 + (0.01 * float64(i))
+// 	}
+
+// 	return sum
+// }
+
+// // don't edit below this line
+
+// func test(numMessages int) {
+// 	fmt.Printf("Sending %v messages\n", numMessages)
+// 	cost := bulkSend(numMessages)
+// 	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+// 	fmt.Println("===============================================================")
+// }
+
+// func main() {
+// 	test(10)
+// 	test(20)
+// 	test(30)
+// 	test(40)
+// 	test(50)
+// }
+
 package main
-
-import (
-	"fmt"
-)
-
-func bulkSend(numMessages int) float64 {
-	sum := 0.0
-	for i := 0; i < numMessages; i++ {
-		sum += 1.0 + (0.01 * float64(i))
-	}
-
-	return sum
-}
-
-// don't edit below this line
-
-func test(numMessages int) {
-	fmt.Printf("Sending %v messages\n", numMessages)
-	cost := bulkSend(numMessages)
-	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
-	fmt.Println("===============================================================")
-}
-
-func main() {
-	test(10)
-	test(20)
-	test(30)
-	test(40)
-	test(50)
-}
